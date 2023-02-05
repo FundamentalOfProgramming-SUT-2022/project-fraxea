335c90800f5822aa5c57342c326ae96b89396754
fraxea <moh788672@gmail.com>
2023-02-05 05:57:25 +0330

    bassse

--- errors_2.h
+++ errors_2.h
@@ -28,62 +28,62 @@ void printOutPut(char *out) {
 }
 // File exist!
 void error1() {
-    printw("File exist!\n"); refresh();
+    printw("\nFile exist!\n"); refresh();
 }
 // Directory doesn't exist!
 void error2() {
-    printw("Directory doesn't exist!\n"); refresh();
+    printw("\nDirectory doesn't exist!\n"); refresh();
 }
 // File doesn't exist!
 void error3() {
-    printw("File doesn't exist!\n"); refresh();
+    printw("\nFile doesn't exist!\n"); refresh();
 }
 // invalid command!
 void error4() {
-    printw("invalid command!\n"); refresh();
+    printw("\ninvalid command!\n"); refresh();
 }
 // The line is not available!
 void error5() {
-    printw("The line is not available!\n"); refresh();
+    printw("\nThe line is not available!\n"); refresh();
 }
 // The line is shorter!
 void error6() {
-    printw("The line is shorter!\n"); refresh();
+    printw("\nThe line is shorter!\n"); refresh();
 }
 // Too many characters to remove!
 void error7() {
-    printw("Too many characters to remove!\n"); refresh();
+    printw("\nToo many characters to remove!\n"); refresh();
 }
 // Too many characters to copy!
 void error8() {
-    printw("Too many characters to copy!\n"); refresh();
+    printw("\nToo many characters to copy!\n"); refresh();
 }
 // Too many characters to cut!
 void error9() {
-    printw("Too many characters to cut!\n"); refresh();
+    printw("\nToo many characters to cut!\n"); refresh();
 }
 // The clipboard is empty!
 void error10() {
-    printw("The clipboard is empty!\n"); refresh();
+    printw("\nThe clipboard is empty!\n"); refresh();
 }
 // These features cannot come together!
 void error11() {
-    printw("These features cannot come together!\n"); refresh();
+    printw("\nThese features cannot come together!\n"); refresh();
 }
 // Cannot find this!
 void error12() {
-    printw("Cannot find this!\n"); refresh();
+    printw("\nCannot find this!\n"); refresh();
 }
 // Replaced succesfully!
 void messege13() {
-    printw("Replaced succesfully!\n"); refresh();
+    printw("\nReplaced succesfully!\n"); refresh();
 }
 // There is no change yet!
 void error14() {
-    printw("There is no change yet!\n"); refresh();
+    printw("\nThere is no change yet!\n"); refresh();
 }
 // Curly braces are incorrect!
 void error15() {
-    printw("Curly braces are incorrect!\n"); refresh();
+    printw("\nCurly braces are incorrect!\n"); refresh();
 }
 
